9da2a6b8465883fab21c123981921c4df885a80f
berryberry189 <berryberry189@naver.com>
2022-09-26 22:54:57 +0900

    코틀린에서 확장함수를 다루는 방법

--- src/com/kotlin_practice/lec16/LocalFuntion.kt
+++ src/com/kotlin_practice/lec16/LocalFuntion.kt
@@ -8,11 +8,11 @@ fun main() {
 
 fun createPerson(firstName: String, lastName: String): Person {
     if(firstName.isEmpty()) {
-        throw IllegalArgumentException("firstName은 비어있을 수 없습니다! 현재 값: $firstName")
+        throw IllegalArgumentException("firstName 은 비어있을 수 없습니다! 현재 값: $firstName")
     }
 
     if(lastName.isEmpty()) {
-        throw IllegalArgumentException("lastName은 비어있을 수 없습니다! 현재 값: $lastName")
+        throw IllegalArgumentException("lastName 은 비어있을 수 없습니다! 현재 값: $lastName")
     }
 
     return Person(firstName, lastName, 1)
